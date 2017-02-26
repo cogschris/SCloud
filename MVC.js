53b6140f6e10456ac9ed67debb871f7e9e14866a
Your Name Here <agui150@usc.edu>
2017-02-25 17:39:11 -0800

    MVC complete for WordCloud

--- MVC.js
+++ MVC.js
@@ -24,27 +24,36 @@ function Word(word, wordCloud) {
   this._wordCloud = wordCloud;
   var freqInCloud;
   var freqInSongMap = new Map();
+
+  var _this = this;
+  // Gen freqInCloud and freqInSongMap
+  // Need to add songs via API calls
+
 }
 
 Word.prototype = {
 
   addSong: function() {
+    // Add song and freq to map
 
   },
 
   getFreqInCloud: function() {
+    // Return freqInCloud
 
   },
 
   getFreqInSong: function() {
+    // Return freqInSong
 
   },
 
   updateFreqInCloud: function() {
-
+    // Calculate and set freqInCloud
   },
 
   updateFreqInSong: function() {
+    // Calculate and set freqInSong
 
   }
 };
@@ -54,7 +63,6 @@ function WordCloud() {
   this._artists = [];
   this._words = [];
 
-  this.wordAdded = new Event(this);
   this.artistAdded = new Event(this);
 }
 
@@ -62,9 +70,6 @@ WordCloud.prototype = {
 
   addWord: function(word) {
     this._words.push(word);
-    this.wordAdded.notify({
-      word: word;
-    });
   },
 
   addArtist: function() {
@@ -87,6 +92,82 @@ WordCloud.prototype = {
 };
 
 // WordCloudView Class - View
-function WordCloudView(wordCloud) {
-  this._wordCloud = 
+function WordCloudView(wordCloud, elements) {
+  this._wordCloud = wordCloud;
+  this._elements = elements;
+
+  this.wordClicked = new Event(this);
+  this.searchButtonClicked = new Event(this);
+  this.addButtonClicked = new Event(this);
+  this.shareButtonClicked = new Event(this);
+
+  var _this = this;
+
+  // Attach WordCloud model listeners
+  this._wordCloud.artistAdded.attach(function() {
+    _this.rebuildCloud();
+  });
+
+  // Attach listeners to HTML controls
+  this._elements.searchButton.click(function() {
+    _this.searchButtonClicked.notify();
+  });
+  this._elements.addButton.click(function() {
+    _this.addButtonClicked.notify();
+  });
+  this._elements.shareButton.click(function() {
+    _this.shareButtonClicked.notify();
+  });
 }
+
+WordCloudView.prototype = {
+  show: function() {
+    this.rebuildCloud();
+  },
+
+  rebuildCloud: function() {
+    // build the cloud...
+  }
+};
+
+// WordCloudController - Controller
+function WordCloudController(wordCloud, wordCloudView) {
+  this._wordCloud = wordCloud;
+  this._wordCloudView = wordCloudView;
+
+  var _this = this;
+
+  this._wordCloudView.wordClicked.attach(function() {
+    // Does this go in here or in Page Manager
+  });
+
+  this._wordCloudView.searchButtonClicked.attach(function() {
+    // Generate new cloud (clear cloud) and add artist
+    _this.clearCloud();
+    _this.addArtist();
+  });
+
+  this._wordCloudView.addButtonClicked.attach(function() {
+    // Add artist to cloud
+    _this.addArtist();
+  });
+
+  this._wordCloudView.shareButtonClicked.attach(function () {
+    // Does this go in here or Page Manager? API call to FB?
+  });
+}
+
+WordCloudController.prototype = {
+  addArtist: function() {
+    // Add artist and words to model
+    var artistName = this._elements.textField.value;
+    if (artistName) {
+      this._wordCloud.addArtist(artistName);
+    }
+  },
+
+  clearCloud: function() {
+      this._wordCloud.clearCloud();
+  }
+
+};
