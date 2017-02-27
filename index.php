a3dd848340b452179e6d254fffa994c1a73a443b
Shaina <shainagi@usc.edu>
2017-02-26 16:38:01 -0800

    search button name

--- index.php
+++ index.php
@@ -25,8 +25,7 @@
 
 <form>
   <div class="form-group">
-<!--     <label for="exampleInputEmail1"> Search </label>
- -->    <input class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter artist">
+    <input class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter artist">
     <!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
   </div>
 
