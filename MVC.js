c144f890a7844c64f239f8660067900985363c9a
student <agui150@usc.edu>
2017-02-27 15:28:14 -0800

    Learning PHP

--- MVC.js
+++ MVC.js
@@ -1,172 +1,172 @@
-// Event Class
-function Event(sender) {
-  this._sender = sender;
-  this._listeners = [];
-}
+// // Event Class
+// function Event(sender) {
+//   this._sender = sender;
+//   this._listeners = [];
+// }
 
-Event.prototype = {
-  attach: function(listener) {
-    this._listeners.push(listener);
-  },
+// Event.prototype = {
+//   attach: function(listener) {
+//     this._listeners.push(listener);
+//   },
 
-  notify: function(args) {
-    var i;
+//   notify: function(args) {
+//     var i;
 
-    for (i = 0; i < this._listeners.length; i+=1) {
-      this._listeners[i](this._sender, args);
-    }
-  }
-};
+//     for (i = 0; i < this._listeners.length; i+=1) {
+//       this._listeners[i](this._sender, args);
+//     }
+//   }
+// };
 
-//Word Class - Model
-function Word(word, wordCloud) {
-  this._word = word;
-  this._wordCloud = wordCloud;
-  var freqInCloud;
-  var freqInSongMap = new Map();
+// //Word Class - Model
+// function Word(word, wordCloud) {
+//   this._word = word;
+//   this._wordCloud = wordCloud;
+//   var freqInCloud;
+//   var freqInSongMap = new Map();
 
-  var _this = this;
-  // Gen freqInCloud and freqInSongMap
-  // Need to add songs via API calls
+//   var _this = this;
+//   // Gen freqInCloud and freqInSongMap
+//   // Need to add songs via API calls
 
-}
+// }
 
-Word.prototype = {
+// Word.prototype = {
 
-  addSong: function() {
-    // Add song and freq to map
+//   addSong: function() {
+//     // Add song and freq to map
 
-  },
+//   },
 
-  getFreqInCloud: function() {
-    // Return freqInCloud
+//   getFreqInCloud: function() {
+//     // Return freqInCloud
 
-  },
+//   },
 
-  getFreqInSong: function() {
-    // Return freqInSong
+//   getFreqInSong: function() {
+//     // Return freqInSong
 
-  },
+//   },
 
-  updateFreqInCloud: function() {
-    // Calculate and set freqInCloud
-  },
+//   updateFreqInCloud: function() {
+//     // Calculate and set freqInCloud
+//   },
 
-  updateFreqInSong: function() {
-    // Calculate and set freqInSong
+//   updateFreqInSong: function() {
+//     // Calculate and set freqInSong
 
-  }
-};
+//   }
+// };
 
-//WordCloud Class - Model
-function WordCloud() {
-  this._artists = [];
-  this._words = [];
+// //WordCloud Class - Model
+// function WordCloud() {
+//   this._artists = [];
+//   this._words = [];
 
-  this.artistAdded = new Event(this);
-}
+//   this.artistAdded = new Event(this);
+// }
 
-WordCloud.prototype = {
+// WordCloud.prototype = {
 
-  addWord: function(word) {
-    this._words.push(word);
-  },
+//   addWord: function(word) {
+//     this._words.push(word);
+//   },
 
-  addArtist: function() {
-    this._artists.push(artist);
-    this.artistAdded.notify({
-      artist: artist
-    });
-  },
+//   addArtist: function() {
+//     this._artists.push(artist);
+//     this.artistAdded.notify({
+//       artist: artist
+//     });
+//   },
 
-  getWords: function() {
-    return [].concat(this._words);
-  },
+//   getWords: function() {
+//     return [].concat(this._words);
+//   },
 
-  getArtists: function() {
-    return [].concat(this._artists);
-  }
+//   getArtists: function() {
+//     return [].concat(this._artists);
+//   }
 
-};
+// };
 
-// WordCloudView Class - View
-function WordCloudView(wordCloud, elements) {
-  this._wordCloud = wordCloud;
-  this._elements = elements;
+// // WordCloudView Class - View
+// function WordCloudView(wordCloud, elements) {
+//   this._wordCloud = wordCloud;
+//   this._elements = elements;
 
-  this.wordClicked = new Event(this);
-  this.searchButtonClicked = new Event(this);
-  this.addButtonClicked = new Event(this);
-  this.shareButtonClicked = new Event(this);
+//   this.wordClicked = new Event(this);
+//   this.searchButtonClicked = new Event(this);
+//   this.addButtonClicked = new Event(this);
+//   this.shareButtonClicked = new Event(this);
 
-  var _this = this;
+//   var _this = this;
 
-  // Attach WordCloud model listeners
-  this._wordCloud.artistAdded.attach(function() {
-    _this.rebuildCloud();
-  });
+//   // Attach WordCloud model listeners
+//   this._wordCloud.artistAdded.attach(function() {
+//     _this.rebuildCloud();
+//   });
 
-  // Attach listeners to HTML controls
-  this._elements.searchButton.click(function() {
-    var item = window.prompt('Add item:', '');
-    _this.searchButtonClicked.notify();
-  });
-  this._elements.addButton.click(function() {
-    _this.addButtonClicked.notify();
-  });
-  this._elements.shareButton.click(function() {
-    _this.shareButtonClicked.notify();
-  });
-}
+//   // Attach listeners to HTML controls
+//   this._elements.searchButton.click(function() {
+//     var item = window.prompt('Add item:', '');
+//     _this.searchButtonClicked.notify();
+//   });
+//   this._elements.addButton.click(function() {
+//     _this.addButtonClicked.notify();
+//   });
+//   this._elements.shareButton.click(function() {
+//     _this.shareButtonClicked.notify();
+//   });
+// }
 
-WordCloudView.prototype = {
-  show: function() {
-    this.rebuildCloud();
-  },
+// WordCloudView.prototype = {
+//   show: function() {
+//     this.rebuildCloud();
+//   },
 
-  rebuildCloud: function() {
-    // build the cloud...
-  }
-};
+//   rebuildCloud: function() {
+//     // build the cloud...
+//   }
+// };
 
-// WordCloudController - Controller
-function WordCloudController(wordCloud, wordCloudView) {
-  this._wordCloud = wordCloud;
-  this._wordCloudView = wordCloudView;
+// // WordCloudController - Controller
+// function WordCloudController(wordCloud, wordCloudView) {
+//   this._wordCloud = wordCloud;
+//   this._wordCloudView = wordCloudView;
 
-  var _this = this;
+//   var _this = this;
 
-  this._wordCloudView.wordClicked.attach(function() {
-    // Does this go in here or in Page Manager
-  });
+//   this._wordCloudView.wordClicked.attach(function() {
+//     // Does this go in here or in Page Manager
+//   });
 
-  this._wordCloudView.searchButtonClicked.attach(function() {
-    // Generate new cloud (clear cloud) and add artist
-    _this.clearCloud();
-    _this.addArtist();
-  });
+//   this._wordCloudView.searchButtonClicked.attach(function() {
+//     // Generate new cloud (clear cloud) and add artist
+//     _this.clearCloud();
+//     _this.addArtist();
+//   });
 
-  this._wordCloudView.addButtonClicked.attach(function() {
-    // Add artist to cloud
-    _this.addArtist();
-  });
+//   this._wordCloudView.addButtonClicked.attach(function() {
+//     // Add artist to cloud
+//     _this.addArtist();
+//   });
 
-  this._wordCloudView.shareButtonClicked.attach(function () {
-    // Does this go in here or Page Manager? API call to FB?
-  });
-}
+//   this._wordCloudView.shareButtonClicked.attach(function () {
+//     // Does this go in here or Page Manager? API call to FB?
+//   });
+// }
 
-WordCloudController.prototype = {
-  addArtist: function() {
-    // Add artist and words to model
-    var artistName = this._elements.textField.value;
-    if (artistName) {
-      this._wordCloud.addArtist(artistName);
-    }
-  },
+// WordCloudController.prototype = {
+//   addArtist: function() {
+//     // Add artist and words to model
+//     var artistName = this._elements.textField.value;
+//     if (artistName) {
+//       this._wordCloud.addArtist(artistName);
+//     }
+//   },
 
-  clearCloud: function() {
-      this._wordCloud.clearCloud();
-  }
+//   clearCloud: function() {
+//       this._wordCloud.clearCloud();
+//   }
 
-};
+// };
---
+++ favorite.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once('classes.php');
+session_start();
+
+if (!isset($_SESSION['favorites'])) {
+	$_SESSION['favorites'] = [];
+}
+if (!isset($_SESSION['words'])) {
+	$_SESSION['words'] = [];
+}
+
+// extract id
+$raw_id = isset($_POST['id']) ? $_POST['id'] : '';
+
+if (preg_match("/blog-post-(\d+)/", $raw_id, $matches)) {
+	$id = $matches[1];
+
+	if (!in_array($id, $_SESSION['favorites'])) {
+		$_SESSION['favorites'][] = $id;
+	}
+
+	$word = $raw_id;
+	if(!isset($_SESSION['words'][$word])) {
+		$wordObject = new Word($word);
+		$wordObject->addSong("Calle Ocho");
+		$_SESSION['words'][$word] = $wordObject;
+
+	} 
+	else {
+		$wordObject = $_SESSION['words'][$word];
+		$wordObject->addSong('Baby');
+	}
+
+	$json = json_encode($_SESSION['words']);
+	echo $json;
+
+} else {
+	echo 'false';
+}
+
+?>
