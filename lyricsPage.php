c822c6935b02d1763dff40298082fa5da8a7a8b5
student <stirumal@usc.edu>
2017-03-01 22:03:03 -0800

    updated styles of songListPAge.php and lyicsPage.php

--- lyricsPage.php
+++ lyricsPage.php
@@ -7,6 +7,7 @@
 		<script type="text/javascript">google.load("jquery", "1.3.2");</script>
 		<script src="APIHandler.js"></script>
 
+		<link href="http://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 		<link rel="stylesheet" href="./css/lyrics.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
@@ -34,23 +35,16 @@
 			<div class = "row">
 				<div class = "col-xs-10 col-xs-offset-1">
 					<h2 id="lyrics">
-
+					 	Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello 
 					</h2>
 				</div>
 			</div>
 
-			<div class = "row">
-				<div class = "col-xs-3 col-xs-offset-3">
-					<div class = "btn pull-right">
-						<div id="back_to_home_btn">Back To Home</div>
-					</div>
-				</div>
-
-				<div class = "col-xs-3">
-					<div class = "btn">
-						<div id="back_to_word_btn">Back To Word</div>
-					</div>
-				</div>
+			<div class = "row" id="buttonRow">
+			
+					<button class="btn btn-lg" id="back_to_WC_btn">Back To Word Cloud</button>
+					
+					<button class="btn btn-lg" id="back_to_songlist_btn">Back To Song List</button>
 
 			</div>
 		</div>
