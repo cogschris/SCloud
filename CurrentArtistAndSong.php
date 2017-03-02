0b74564c0bb471ca17ef5d7813361bfc0020955c
Emanuel Azage <eazage@usc.edu>
2017-03-01 22:51:26 -0800

    grabbing song and artist name in from php server

---
+++ CurrentArtistAndSong.php
@@ -0,0 +1,25 @@
+<?php 
+	session_start();
+
+	if(!isset($_SESSION['words'])) { 
+	  	$_SESSION['words'] = [];
+	}
+	if(!isset($_SESSION['topWords'])) { 
+	  	$_SESSION['topWords'] = [];
+	}
+	if(!isset($_SESSION['artists'])) { 
+	  	$_SESSION['artists'] = [];
+	}
+	if(!isset($_SESSION['totalWordsInCloud'])) { 
+		$_SESSION['totalWordsInCloud'] = 0;
+	}
+	if(!isset($_SESSION['currentSong'])) { 
+		$_SESSION['currentSong'] = "Baby"; 
+	}
+	if(!isset($_SESSION['currentArtist'])){
+		$_SESSION['currentArtist'] = "Artist for Da Baby";
+	}
+
+	$results = Array($_SESSION['currentSong'], $_SESSION['currentArtist']);
+	echo $results[0]."1996!&#@($@&)#^&*:-".$results[1];
+?>
