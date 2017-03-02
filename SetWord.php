bb56076185719cd061e8fd1a895052001dd5bfac
student <agui150@usc.edu>
2017-03-02 02:29:20 -0800

    f

---
+++ SetWord.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once('Word.php');
+// Start the session
+session_start();
+
+if(!isset($_SESSION['words'])) { 
+  	$_SESSION['words'] = [];
+}
+if(!isset($_SESSION['topWords'])) { 
+  	$_SESSION['topWords'] = [];
+}
+if(!isset($_SESSION['currentWord'])) { 
+  	$_SESSION['currentWord'];
+}
+if(!isset($_SESSION['artists'])) { 
+  	$_SESSION['artists'] = [];
+}
+if(!isset($_SESSION['totalWordsInCloud'])) { 
+	$_SESSION['totalWordsInCloud'] = 0;
+}
+if(!isset($_SESSION['currentSong'])) {
+	$_SESSION['currentSong'] = 'Baby';
+}
+
+$raw_word = isset($_POST['word']) ? $_POST['word'] : '';
+$_SESSION['currentWord'] = $raw_word;
+echo $_SESSION['currentWord'];
+
+?>
