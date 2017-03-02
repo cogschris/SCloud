d73ae05e456fa638094e71e2c653c0d688655002
student <stirumal@usc.edu>
2017-03-01 22:56:57 -0800

    made table smaller

--- songListPage.php
+++ songListPage.php
@@ -20,7 +20,7 @@
       </div>
 
      <div class ="row">
-      <div class = "col-xs-12">
+      <div class = "col-xs-8 col-xs-offset-2">
         <table class="table table-striped" id="songListTable">
        
           <thead>
